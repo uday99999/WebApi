09f2b18322913af541eba1d82571ff656e494c1e
uday99 <udaypeddu9999@gmail.com>
2020-03-04 11:40:02 -0600

    Add project files.

---
+++ WebApi/Models/PaymentDetailContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WebApi.Models
+{
+    public class PaymentDetailContext: DbContext
+    {               //constrcutor
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options) : base(options)///ctor double tb
+        {
+
+        }
+            public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+    }
+
